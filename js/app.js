b3189086fed88560fc55f47387d85c54c12407c0
eSSeGH <ste.virgadaula@gmail.com>
2023-02-09 18:07:43 +0100

    EDIT+STYLE+JS section level-bar & grid added

---
+++ js/app.js
@@ -0,0 +1,44 @@
+console.log("Campo Minato")
+
+const btnPlay = document.getElementById("btn-play")
+
+btnPlay.addEventListener("click", function (){
+
+    let sideGrid = document.getElementById("level-form").value
+
+    while (isNaN(sideGrid) || sideGrid > 50 || sideGrid < 1) {
+        sideGrid = prompt("Inserisci un numero compreso tra 1 e 50")
+    }
+
+    let cellNum = sideGrid**2
+
+    const gridEl = document.querySelector(".grid")
+
+    for (i=0; i < cellNum; i++) {
+        let num = i + 1
+        console.log(i + 1)
+    
+        const cellEl = document.createElement("div")
+        console.dir
+    
+        cellEl.className = "cell"
+        cellEl.style.width = `calc(100%/ ${sideGrid})`
+        cellEl.innerHTML = num
+    
+        gridEl.append(cellEl)
+    
+        cellEl.addEventListener("click", function () {
+            console.log(`click ${num}`)
+        })
+    }
+})
+
+
+
+// generare le celle nella griglia
+
+
+
+
+
+
