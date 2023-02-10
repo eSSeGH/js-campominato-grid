194b7f5604f63f585e3d32ccfd8b7f5ba1a36c31
eSSeGH <ste.virgadaula@gmail.com>
2023-02-10 11:33:33 +0100

    JS code divided into funcs EDIT form placeholder

--- js/app.js
+++ js/app.js
@@ -2,15 +2,35 @@ console.log("Campo Minato")
 
 const btnPlay = document.getElementById("btn-play")
 
-btnPlay.addEventListener("click", function (){
+const gridEl = document.querySelector(".grid")
+
+btnPlay.addEventListener("click", startGame)
+
+
+
+
+
+
+
+// FUNZIONI
+
+function resetGame() {
+    // azzerare il punteggio
+    // svuotare la griglia
+    gridEl.innerHTML = ""
+    // eliminare eventuali messaggi di game over
+}
+
+function startGame() {
     console.log("click play")
+
+    resetGame()
 
     let sideGrid = document.getElementById("level-form").value
     let exclamationPoint = "!"
-    
 
-    while (isNaN(sideGrid) || sideGrid > 50 || sideGrid < 10) {
-        sideGrid = prompt(`Inserisci un numero compreso tra 1 e 50 ${exclamationPoint} `)
+    while (isNaN(sideGrid) || sideGrid > 30 || sideGrid < 10) {
+        sideGrid = prompt(`Inserisci un numero compreso tra 10 e 30 ${exclamationPoint} `)
         exclamationPoint += "!!!"
 
         if (exclamationPoint === "!!!!!!!!!!") {
@@ -21,35 +41,43 @@ btnPlay.addEventListener("click", function (){
         }
     }
 
-    const gridEl = document.querySelector(".grid")
-    gridEl.innerHTML = ""
-
-    let cellNum = sideGrid**2
-
-    for (i=0; i < cellNum; i++) {
-        let num = i + 1
-        console.log(i + 1)
-    
-        const cellEl = document.createElement("div")
-        console.dir
-    
-        cellEl.className = "cell"
-        cellEl.style.width = `calc(100%/ ${sideGrid})`
-        cellEl.style.height = `calc(100%/ ${sideGrid})`
-        cellEl.innerHTML = num
-    
-        gridEl.append(cellEl)
-    
-        cellEl.addEventListener("click", function () {
-            console.log(`click ${num}`)
-            cellEl.style.backgroundColor = "rgb(255, 140, 0)"
-        })
-    }
+    createGrid(sideGrid)
 
     btnPlay.innerHTML = "RESTART"
-})
+}
 
+function createGrid(sideOfGrid) {
 
+    let cellNum = sideOfGrid**2
+
+    for (i=0; i < cellNum; i++) {
+
+        let num = i + 1
+        console.log(i + 1)
+
+        const cellEl = document.createElement("div")
+        console.dir
+
+        cellEl.className = "cell"
+        cellEl.style.width = `calc(100%/ ${sideOfGrid})`
+        cellEl.style.height = `calc(100%/ ${sideOfGrid})`
+        cellEl.innerHTML = num
+
+        gridEl.append(cellEl)
+
+        cellEl.addEventListener("click", onClick)
+        console.log(`click ${num}`)
+    }
+}
+
+function onClick(event) {
+
+    const clickedCell = event.target
+
+    clickedCell.style.backgroundColor = "rgb(255, 140, 0)"
+
+    clickedCell.removeEventListener("click", onClick)
+}
 
 
 
